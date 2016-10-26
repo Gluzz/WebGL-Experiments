1c6d6bf496e943e053d1db592f100bcf17a983a8
OMaese <72632pc@gmail.com>
2016-10-26 15:14:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 console.log('console test'); //console print test
 
 var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+	camera.position.x = 5;
 
-			var cube = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { 
-			color: 0xffffff,
-			wireframe: true,
-			} );
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	var cube = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { 
+	color: 0xffffff,
+	wireframe: true,
+} );
 			
-			var gridHelper = new THREE.GridHelper( 50, 10 );
-			scene.add( gridHelper );
+var gridHelper = new THREE.GridHelper( 50, 10 );
+scene.add( gridHelper );
 			
-			var cube = new THREE.Mesh( cube, material );
-			var GridHelper = new THREE.Mesh( gridHelper, material );
-			scene.add( cube );
+	var cube = new THREE.Mesh( cube, material );
+	var GridHelper = new THREE.Mesh( gridHelper, material );
+scene.add( cube );
 			
-			var loader = new THREE.JSONLoader();
+var loader = new THREE.JSONLoader();
 
-			var render = function () {
-				requestAnimationFrame( render );
+var render = function () {
+	requestAnimationFrame( render );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-				gridHelper.rotation.x += 0.0;
-				gridHelper.rotation.y += 0.0;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	gridHelper.rotation.x += 0.0;
+	gridHelper.rotation.y += 0.0;
 
-				renderer.render(scene, camera);
-			};
+	renderer.render(scene, camera);
+};
 
 render();
