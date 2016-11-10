888a4130de5876cabb77c87d2bff98996a7fc1a8
OMaese <72632pc@gmail.com>
2016-11-10 10:57:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var scene = new THREE.Scene();
 	camera.position.x = 0;
 	camera.rotation.x = -0.2
 
+	cube.position.x = 0
+	cube.position.y = 12
+	cube.position.z = 0
 
 var renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -36,6 +39,7 @@ var render = function () {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+	
 	gridHelper.rotation.x += 0.0;
 	gridHelper.rotation.y += 0.0;
 
